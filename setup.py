927632de33c0933ee143b94ed77ab224c3e332bf
Arbona <jeanmichel.arbona@gmail.com>
2017-02-02 09:27:05 +0100

    Made it a python repository

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='replication',
+      version='0.1',
+      description='Replicating DNA spatio temporal programm',
+      url='https://github.com/jeammimi/replication.git',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['replication'],
+      zip_safe=False)
